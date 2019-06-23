cbc43f248ff133dead7e10321271eec104d3afb6
Ved Prakash Kashyap <vedprakashkashyap@Veds-MacBook-Pro.local>
2019-06-24 00:21:41 +0530

    added setup for my xcode cpp programs

---
+++ ACTIVE/template.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[1000];
+int main(){
+    freopen("input.txt", "r+", stdin);
+    int t,n;
+    cin>>t;
+    while (t--) {
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
