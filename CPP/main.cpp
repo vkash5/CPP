c1bdc3cd330ef5f7c69e93ac88ae07b45dba38dd
Ved Kashyap <vedprakashkashyap@Veds-MacBook-Pro.local>
2019-06-23 18:27:30 +0530

    Initial Commit

---
+++ CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP
+//
+//  Created by Ved Prakash Kashyap on 23/06/19.
+//  Copyright © 2019 initiator. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
