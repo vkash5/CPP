1bb8ef3527b298073d43d7aa4e0ee6b0aa84dc56
Ved <prakash.ved121@gmail.com>
2019-06-25 11:09:15 +0530

    puzzle working

--- ALL/puzzle9.cpp
+++ ALL/puzzle9.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int dx[] = {0,1,0,-1};
+int dy[] = {1,0,-1,0};
+int revDir[] = {2,3,0,1};
+struct Moves {
+    int x;
+    int y;
+    int dir;
+};
+Moves moves[1000000];
+int movesCnt;
 const int n = 3;
 int arr[n][n];
 int valid[n][n];
 bool visited[n][n];
-vector<pair<int,int>> pos;
-vector<int> dir;
-int dx[] = {0,1,0,-1};
-int dy[] = {1,0,-1,0};
 
-void print(int arr[n][n]){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-}
-
-bool check(){
+bool check(int x, int y){
+    if(valid[x][y] != arr[x][y]) return false;
+    
     int matched = true;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -40,36 +40,36 @@ bool check(){
     }
 }
 
-bool getsoln(pair<int,int> current, pair<int,int> parent, vector<pair<int,int>> &pos, vector<int> &dir){
-    if(check()){
+bool getsoln(int x, int y){
+    if(check(x,y)){
         return true;
     }
     for(int i=0;i<4;i++){
-//        visited[x][y] = true;
-        int x = current.first;
-        int y = current.second;
+        if(visited[x][y] == true){
+            continue;
+        }
         int xx = x + dx[i];
         int yy = y + dy[i];
         if(xx<0 || yy<0 || xx>=n || yy>=n){
             continue;
         }
         
-        if(xx!=parent.first || yy!=parent.second){
+        if(!visited[x][y]){
             swap(valid[x][y],valid[xx][yy]);
-//            visited[x][y] = true;
-            pos.push_back(make_pair(xx, yy));
-            dir.push_back(i);
-//            print(valid);
-//            cout<<endl;
-            bool res = getsoln(make_pair(xx,yy),make_pair(x,y),pos,dir);
+            visited[x][y] = true;
+            moves[movesCnt].x = xx;
+            moves[movesCnt].y = yy;
+            moves[movesCnt].dir = revDir[i];
+            movesCnt++;
+            
+            bool res = getsoln(xx,yy);
             if(res){
                 return true;
             }
             
             swap(valid[x][y],valid[xx][yy]);
-//            visited[x][y] = false;
-            pos.pop_back();
-            dir.pop_back();
+            visited[x][y] = false;
+            movesCnt--;
         }
     }
     return false;
@@ -81,6 +81,7 @@ int main(){
     cin>>t;
     while (t--) {
         int c = 1;
+        movesCnt = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 cin>>arr[i][j];
@@ -94,16 +95,13 @@ int main(){
                 visited[i][j] = false;
             }
         }
-        pos.clear();
-        dir.clear();
-        pair<int,int> currnet,parent;
-        currnet = make_pair(2, 2);
-        parent = make_pair(2, 2);
-        getsoln(currnet,parent,pos,dir);
+
+        getsoln(2,2);
         
-        for(int i=0;i<pos.size();i++){
-            cout<<"( "<<pos[i].first<<" "<<pos[i].second<<" ) -> "<<dir[i]<<endl;
+        for(int i=0;i<movesCnt;i++){
+            cout<<"( "<<moves[i].x<<", "<<moves[i].y<<" ) -> "<<moves[i].dir<<endl;
         }
+        cout<<endl;
     }
     
     return 0;
